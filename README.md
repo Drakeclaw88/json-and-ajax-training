cfee82b75832210ed27ac2066ce6d62869232a98
Oscar Åberg <oscar.aberg1@hotmail.com>
2020-09-26 14:32:06 +0200

    added json functionality with buttonclick

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # json-and-ajax-training
 training how to use json and ajax with making an easy webpage
+
+this is using xmlhttprequest as a metod
+
--- main.js
+++ main.js
@@ -1,8 +1,42 @@
+let pageCounter = 1;
+let buttonFetchAnimals = document.getElementById("fetchAnimals");
+let animalView = document.getElementById("animalInfo");
 
-let request = new XMLHttpRequest();
-request.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
-request.onload = function(){
+buttonFetchAnimals.addEventListener("click", function () {
+
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json')
+    request.onload = function () {
+        let petData = JSON.parse(request.responseText);
+        addHTML(petData);
+
+    };
+    request.send();
+    pageCounter++;
+    if (pageCounter > 3) {
+
+        buttonFetchAnimals.classList.add("hide-me");
+        
+    }
+});
+
+function addHTML(data) {
+    let htmlString = "";
+
+    for (i= 0; i < data.length; i++){
+        htmlString += "<p>" + data[i].name +  " is a " + data[i].species + " that likes to eat ";
+
+        for (let ii = 0; ii < data[i].foods.likes.length; ii++) {
+            htmlString += data[i].foods.likes[ii];
+            
+        }
+
+    };
+        
+    animalView.insertAdjacentHTML('beforeend', htmlString);
 
 
-};
-request.send();
+
+}
+
+
